c722ca41a886f207cb20e0085b9cf654a6f37d1c
Keith Crooc <keith.crooc@gmail.com>
2020-10-19 09:46:40 -0400

    Added prompts to ask what user would like in Password. Results are added to an Array

--- script.js
+++ script.js
@@ -9,16 +9,29 @@ const passwordSpecial = '';
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  const passLength = prompt("How long would you like your password to be? :");
+  const wantsUpper = confirm("Do you want UPPERCASE characters?");
+  const wantsLower = confirm("Do you want lowercase characters?");
+  const wantsSpecial = confirm("Do you want Special characters?");
+  const wantsNumber = confirm("Do you want numbers?");
+
+
+  const passCriteria = [wantsLower, wantsUpper, wantsSpecial, wantsNumber];
+
+  const criteriaArray = [{wantsLower}, {wantsUpper}, {wantsSpecial}, {wantsNumber}]
+
+  console.log(passCriteria);
+  console.log(criteriaArray);
+
+  var password = generatePassword(passLength, wantsLower, wantsUpper, wantsSpecial, wantsNumber);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-function generatePassword() {
-  
-  
+function generatePassword(length, lower, upper, special, number) {
+
 }
 
 const randomGen = {
@@ -49,7 +62,6 @@ function getRandomSpecial() {
   return specialChar[Math.floor(Math.random() * specialChar.length)]
 }
 
-console.log(getRandomSpecial())
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
